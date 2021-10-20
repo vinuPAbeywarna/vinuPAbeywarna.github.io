f74a4f29eabf794a1192883598268af89cd01872
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2021-10-21 02:51:30 +0530

    build

---
+++ pages/projects.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <h1>Projects</h1>
+    <v-btn to="/">TOHOME</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "projects"
+}
+</script>
+
+<style scoped>
+
+</style>
